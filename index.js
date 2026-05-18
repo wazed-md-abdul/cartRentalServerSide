b30650256174a7b7900046cb30d08a15ff8e9ffe
Md Abdul Wazed <wazedjiad@gmail.com>
2026-05-19 04:08:39 +0800

    all dependencies and import

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const dotend = require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
+const app = express();
+app.use(cors());
+app.use(express.json());
+const uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+ 
+const JWKS = createRemoteJWKSet(new URL(`${process.env.BASE_URL}/api/auth/jwks`));
+async function run() {
+  try {
+
+
+
+
+   
+    await client.connect();
+
+    const db = client.db("DriveLoop");
+    const carsCollection = db.collection("cars");
+    const bookingCollection = db.collection("booking");
+
+    app.get('/cars', async (req, res) => {
+        const result = await carsCollection.find().toArray();
+        res.send(result);
+    })
+        
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('server is running')
+})
+
+
+
+
+
+app.listen(PORT ,() => {
+    console.log(`Example app listening on port ${PORT}`)
+})
